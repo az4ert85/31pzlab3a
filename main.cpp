9443d8bc6a450ab6415d177c397c61db0529fea5
Wo1shebnik <klasnyjhlop@gmail.com>
2021-04-14 13:07:38 +0300

    lab

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include"rectangularMatrix.h"
+int
+main ()
+{
+  actionsOnRectangularMatrix < int > variant8;
+  variant8.printMatrix();
+  variant8.rowsWithoutZeros();
+  variant8.maxNumberWitchWasFindTwoTimes();
+  variant8.taskX();
+  variant8.printMatrix();
+  return 0;
+}
